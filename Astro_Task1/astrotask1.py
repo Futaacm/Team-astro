7f5cacc1345eb527b86fff083265454192ef5519
chris <christopheradelusi@gmail.com>
2017-11-17 16:43:14 +0100

    a new commit

---
+++ Astro_Task1/astrotask1.py
@@ -0,0 +1,9 @@
+print"The program that print maximum and minimum of set of numbers"
+h=input("Enter a set of numbers seperated by comma:")
+k=[(h)]
+
+bignum = max(h)
+smallnum = min(h)
+print "The maximum number is", bignum
+print "The minimum number is", smallnum
+raw_input("Press<enter>")
